edddce02533bf39b21cd1983a40903fe2be49181
Abhi <koolavi16@gmail.com>
2022-02-03 16:21:25 +0530

    c

--- LinkedList/CircularLinkedList/insertion.cpp
+++ LinkedList/CircularLinkedList/insertion.cpp
@@ -13,6 +13,67 @@ public:
 void addToEmptyList(Node **last, int data)
 {
     Node *new_node = new Node(data);
-    new_node->next = new_node;
     *last = new_node;
+    (*last)->next = (*last);
+}
+void insertEnd(Node **last, int data)
+{
+    if (*last == NULL)
+    {
+        addToEmptyList(last, data);
+        return;
+    }
+
+    Node *new_node = new Node(data);
+    new_node->next = (*last)->next;
+    (*last)->next = new_node;
+    *last = new_node;
+}
+void insertFront(Node **last, int data)
+{
+    if (*last == NULL)
+    {
+        addToEmptyList(last, data);
+        return;
+    }
+
+    Node *new_node = new Node(data);
+    new_node->next = (*last)->next;
+    (*last)->next = new_node;
+}
+void traverse(Node *last)
+{
+    Node *p;
+
+    // If list is empty, return.
+    if (last == NULL)
+    {
+        cout << "List is empty." << endl;
+        return;
+    }
+
+    // Pointing to first Node of the list.
+    p = last->next;
+
+    // Traversing the list.
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+
+    } while (p != last->next);
+}
+int main()
+{
+    Node *last = NULL;
+
+    addToEmptyList(&last, 6);
+    insertFront(&last, 4);
+    insertFront(&last, 2);
+    insertEnd(&last, 8);
+    insertEnd(&last, 12);
+
+    traverse(last);
+
+    return 0;
 }
